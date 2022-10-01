21a91cdeef54386ded0832ce5af9811220ddd014
guru <gc66357@gmail.com>
2022-10-01 21:46:59 +0530

    initialize files

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert("yes")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLFkiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvX2xpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQoXCJ5ZXNcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
