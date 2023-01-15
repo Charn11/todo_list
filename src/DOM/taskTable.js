51bf5eb9111f0ebfd1d5ba31e0f5f377b5156698
guru <gc66357@gmail.com>
2023-01-15 21:08:51 +0530

    edit tables

--- src/DOM/taskTable.js
+++ src/DOM/taskTable.js
@@ -10,21 +10,37 @@ let createTable = () => {
     let tasktr = [], tasktd = [];
     if(taskCount==0)
     {
-
+        taskTable.innerText = "NO ACTIVE TASKS"
     }
     else{
+        taskTable.innerText = ""
         for(let i=taskCount-1;i<taskCount;i++)
         {
             tasktr[i] = document.createElement('tr');
+            tasktr[i].setAttribute('class','tasktr');
             taskTable.appendChild(tasktr[i]);
-                    for(let j=0; j<4; j++)
+                    for(let j=0; j<5; j++)
                     {
                         tasktd[j] = document.createElement('td');
                         if(j==0){
                             tasktd[j].innerText = crtTask[i].title;
                         }
-                        if(j==1){
+                        else if(j==1){
                             tasktd[j].innerText = crtTask[i].description;
+                        }
+                        else if(j==2)
+                        {
+                            tasktd[j].innerText = crtTask[i].duedate;
+                        }
+                        else if(j==3)
+                        {
+                            tasktd[j].innerText = crtTask[i].priority;
+                        }
+                        else if(j==4)
+                        {
+                            let tdImg = document.createElement('img');
+                            tdImg.setAttribute("src", "../images/delete.svg")
+                            tasktd[j].appendChild(tdImg);
                         }
                         tasktr[i].appendChild(tasktd[j]);
                     }
